fbbda27b561fe34a92268ae232bd5d8b346e6113
Dustin Michael Smith <Dustinmichaelsmith03@gmail.com>
2020-06-01 20:10:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,141 +1,102 @@
-
+#  STEP 1 -Import the necessary libraries
 
 import cbpro, datetime, numpy, time
  
-def authorized():
-   apiKey = 'your api key here'
-   apiSecret = 'your api secret here'
-   passphrase = 'passphrase goes here'
-    auth_client = cbpro.AuthenticatedClient(apiKey,apiSecret,passphrase)
-    return auth_client
+#  STEP 2 - GET AUTHORIZED
+
+apiKey = 'your api key here'
+apiSecret = 'your api secret here'
+passphrase = 'passphrase goes here'
+auth_client = cbpro.AuthenticatedClient(apiKey,apiSecret,passphrase)
 
 
-def getCurrencyInput():
- x = input('Please enter the coin you would like to trade: '+ '') + '-USD'
- return x
-currency = getCurrencyInput()
+# STEP 3 -  Pick a pair to trade 
 
-def getAccountId(cur):
- x = auth_client.get_accounts()
- for account in x:
-  if account['currency'] == cur:
+coin = input('Coin:'+'')
+fiat = input('Fiat:'+'')
+currency = (coin+'-'+fiat)
+
+#  STEP 4 - Get the ID's for the chosen pair 
+
+def account(iD):
+ for account in auth_client.get_accounts():
+  if account['currency'] == iD:
    return account['id']
 
-pairIdA = getAccountId(currency[:3])# Get the currency's specific ID
-pairIdB = getAccountId('USD'[:3])# Get the currency's specific ID
+# STEP 5 - Get the chosen pair's current price
 
-def getBuyPower():
- bp = float(auth_client.get_account(pairIdB)['available']) 
- buyingPower = round(bp-1,2)
- return buyingPower
+startingPrice = float(auth_client.get_product_ticker(product_id=currency)['price'])
 
-def getSellPower():
- sellingPower = float(auth_client.get_account(pairIdA)['available'])
- return sellingPower
-    
+# STEP 6 - Get the chosen pair's current balances
+startingCoin = float(auth_client.get_account(account(coin[:3]))['available'])
+startingDollar = float(auth_client.get_account(account(fiat[:3]))['available'])
+startingValue = float(startingDollar + (startingCoin * startingPrice))
 
-def getPrice():
- productTicker = auth_client.get_product_ticker(product_id=currency)
- currentPrice = float(productTicker['price'])
- return currentPrice
+#  STEP 7 - Set the prices to later distinguish high from low and make profitable trades.
 
+lastBuy = startingPrice
+lastSell = startingPrice
 
-def determineBuy():
- if firstTrade == True and (getPrice() < startPrice) == True:
-  buy = True
- else:
-  if firstTrade == False and (getPrice() < (lastSell - (lastSell * 0.005))) == True:
-   buy = True
-  else:
-   buy = False
- return buy
+# STEP 8 - Set soldLast to none so that we start looking to buy or sell, turn of later to prevent double sell.
 
-def determineSell():
- if firstTrade == True and (getPrice() > startPrice) == True:
-  sell = True
- else:
-   if firstTrade == False and (getPrice() > (lastBuy + (lastBuy * 0.005))) == True:
-    sell = True
-   else:
-    sell = False
- return sell
+soldLast = None
 
-def placeBuy():
-
- if determineBuy() == True and getCoppockSignal() == True :
-  #auth_client.place_market_order(product_id=currency, side='buy', funds=str(funding))
-  lastBuy = getPrice()
-  firstTrade = False
-  print(iteration,'buy!',timestamp, lastBuy, sellingPower, buyingPower)
-  time.sleep(300)
-  
-def placeSell():
- 
- if determineSell() == True and getCoppockSignal() == True :
-  #auth_client.place_market_order(product_id=currency,side='sell',size=str(funding))
-  firstTrade = False
-  lastSell = getPrice()
-  print(iteration,'sell!',timestamp, lastSell, float(sellingPower), buyingPower)
-  time.sleep(300)
-def getCoppockSignal():
-    historicData = auth_client.get_product_historic_rates(currency, granularity=candle)
-    historicRates = numpy.squeeze(numpy.asarray(numpy.matrix(historicData)[:,4]))
-    ROC11 = numpy.zeros(13)
-    ROC14 = numpy.zeros(13)
-    ROCSUM = numpy.zeros(13)       
-    for ii in range(0,13):
-        ROC11[ii] = (100*(historicRates[ii]-historicRates[ii+11]) / float(historicRates[ii+11]))
-        ROC14[ii] = (100*(historicRates[ii]-historicRates[ii+14]) / float(historicRates[ii+14]))
-        ROCSUM[ii] = ( ROC11[ii] + ROC14[ii] )
-
-        time.sleep(1)
-    coppock = numpy.zeros(4)
-    for ll in range(0,4):
-        coppock[ll] = (((1*ROCSUM[ll+9]) + (2*ROCSUM[ll+8]) + (3*ROCSUM[ll+7]) \
-        + (4*ROCSUM[ll+6]) + (5*ROCSUM[ll+5]) + (6*ROCSUM[ll+4]) \
-        + (7*ROCSUM[ll+3]) + (8*ROCSUM[ll+2]) + (9*ROCSUM[ll+1]) \
-        + (10*ROCSUM[ll])) / float(55))
-
-    coppockD1 = numpy.zeros(3)
-    for mm in range(3):
-        coppockD1[mm] = coppock[mm] - coppock[mm+1]
-    if (coppockD1[0]/abs(coppockD1[0])) == -1.0 and (coppockD1[1]/abs(coppockD1[1])) == 1.0:
-        signal = True
-
-    else:
-        signal = False
-    return signal
-
-
-startPrice = getPrice()
-
-sellingPowerStart  = getSellPower()
-buyingPowerStart  = getBuyPower()
-lastBuy = getPrice()
-lastSell = getPrice()
+# STEP 9 - Iinitaite the loop, start iteration count, and trade count, begin loop.
 
 trade = True
-firstTrade = True
-
-candle = 300
-
 tradeCount = 0
-iteration = 1
+iteration = 0
 
 while trade == True:
-
- buyingPower = getBuyPower()
- sellingPower  = getSellPower()  
-
- now = datetime.datetime.now()
- timestamp = (now.strftime('%H:%M '))
-
- placeBuy()
- placeSell()
- 
- print(iteration,'firstTrade:', firstTrade, 'signal :',getCoppockSignal(),'determineBuy():',determineBuy(),'determineSell() :',determineSell(),'startPrice:', startPrice, 'getPrice:', getPrice(),'sellingPower', sellingPower,'buyingPower', buyingPower)
-
+  
+# STEP 10 - Count a new iteration define fee(desired gain), price, funding, portfolio value, and running total of profit 
+  
+    iteration += 1
+    fee = (0.01)
+    price = float(auth_client.get_product_ticker(product_id=currency)['price'])
+    x = float(auth_client.get_account(account(coin[:3]))['available'])
+    owned = round(x-(x*0.25),2)
+    y = float(auth_client.get_account(account(fiat[:3]))['available'])
+    funding = round(y-1,2)
+    currentValue = float(y + (x * price))
+    profit = round((currentValue - startingValue),5)
     
- iteration = iteration + 1
- time.sleep(300)
+# STEP 11 - Place a market sell order if the onditions are met, then tell me, update soldlast variable, count the new trade.
+    
+    if (price > lastBuy + (lastBuy * fee)):
+        if soldLast == False or None:
+            auth_client.place_market_order(product_id = currency, side='sell', size = str(owned))
+            lastSell =  float(auth_client.get_product_ticker(product_id=currency)['price'])
+            print('sell!')
+            soldLast = True
+            tradeCount += 1
+            
+# STEP 12 - Place a market buy order if the onditions are met, then tell me, update soldlast variable, count the new trade.
 
+    if (price < lastBuy - (lastBuy * fee)):
+        if soldLast == True or None:
+            auth_client.place_market_order(product_id = currency, side='buy', funds = str(funding))
+            lastBuy =  float(auth_client.get_product_ticker(product_id=currency)['price'])
+            print('buy!')
+            soldLast = False
+            tradeCount += 1
+            
+# STEP 13 - Tell me all about everything
+
+    print(currency,
+          'dollar:', y,
+          'coin:', x,
+          'starting value:',startingValue,
+          'current value:', currentValue,
+          'profit:', profit,
+          'last sell', lastSell,
+          'desired buy:',(lastBuy - (lastBuy * fee)),
+          'price:', price,
+          'lastBuy:',lastBuy,
+          'desiredSell:',(lastBuy + (lastBuy * fee)),
+          'tradeCount:', tradeCount,
+          'iteration', iteration)
+    
+# STEP 14 - Take a few seconds to breathe, then do it again.
+    
+    time.sleep(10)
